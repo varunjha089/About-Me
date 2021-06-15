3199bfcda89afdfcff7801e863cbed6821ad29d3
VARUN KUMAR <varunjha089@gmail.com>
2021-06-15 19:16:32 +0530

    Migrated to data binding for fast performance while running.

--- app/src/main/java/com/tuneyourcode/aboutme/MainActivity.kt
+++ app/src/main/java/com/tuneyourcode/aboutme/MainActivity.kt
@@ -5,29 +5,31 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.tuneyourcode.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        // OnClickListener for button.
-        findViewById<Button>(R.id.done_button).setOnClickListener {
+        binding.doneButton.setOnClickListener {
             addNickname(it)
         }
     }
 
     private fun addNickname(view: View){
-        val editText = findViewById<EditText>(R.id.nick_name_edit)
-        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
 
-        nicknameTextView.text = editText.text
-        editText.visibility = View.GONE
-        view.visibility = View.GONE
-        nicknameTextView.visibility = View.VISIBLE
+        binding.apply {
+            binding.nicknameText.text = binding.nickNameEdit.text
+            invalidateAll()
+            binding.nickNameEdit.visibility = View.GONE
+            binding.doneButton.visibility = View.GONE
+            binding.nicknameText.visibility = View.VISIBLE
+        }
 
         // Hiding the input text and keyboard
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
